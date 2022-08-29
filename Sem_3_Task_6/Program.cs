86f8f2b78146299dd6bc2def89263ec5a3a32081
Maks <nordmax69@gmail.com>
2022-08-30 00:04:23 +0700

    Сделал задание 6 семинара 3

---
+++ Sem_3_Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из 8 элементов, заполненный нулями и еденицами и вывести их на экран
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Задаём массив из 8 элементов с нулями и единицами");
+int [] array =new int[8];
+void ZeroAndOne(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+        Console.Write($"{array[i]} ");
+
+    }
+}
+ZeroAndOne(array);
